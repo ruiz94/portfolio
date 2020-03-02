ebd237cc986c042225e9fe9f261f67668ad49952
ruiz94 <armando_ruiizz@hotmail.com>
2020-03-02 10:44:12 -0600

    agregar archivos faltantes

---
+++ js/script.js
@@ -0,0 +1,22 @@
+// $(function(){
+//     var win = $(window)
+//     var pos = 5 * 16
+//     win.scroll(function(){
+//         if(win.scrollTop() <= pos)
+//             $('.portafolio .header').removeClass('fijar')
+//         else
+//             $(".portafolio .header").addClass('fijar')
+//     })
+// })
+const navToggle = document.querySelector('.nav-toggle')
+const nav__links = document.querySelectorAll('.nav__link')
+// console.log(navToggle)
+
+navToggle.addEventListener('click', function(){
+    document.body.classList.toggle('nav-open')
+})
+nav__links.forEach(el => {
+    el.addEventListener('click', function(){
+        document.body.classList.remove('nav-open')
+    })
+})
